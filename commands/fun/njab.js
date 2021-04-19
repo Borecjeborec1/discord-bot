ae4c0ff1459e0ec3c94b8c3a0bfa9dd8edda5caf
borecjeborec1 <robingrundel@seznam.cz>
2021-04-19 07:54:47 +0200

    changed PDF

--- commands/fun/njab.js
+++ commands/fun/njab.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'njab',
     description: 'Sends german arbeitsbuch',
     execute(msg, args, client) {
-      msg.channel.send("",{files:["./database/Němčina-řešení-AB.pdf.pdf"]});
+      msg.channel.send("",{files:["./database/Němčina-řešení-AB.pdf"]});
     },
   };
